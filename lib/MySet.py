a377bc2569deb216cbddb57de3d44308a9b9a712
tinamanga <tinamanga@gmail.com>
2025-05-23 01:17:27 +0300

    dentify the use cases for a Set. Implement common methods for a Set

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,28 @@
 class MySet:
+    def __init__(self,enumerable=[]):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+
+    def size(self):
+        return len(self.dictionary)
+
+    def clear(self):
+        self.dictionary.clear()
+        return self
+
+    def __str__(self):
+        # Creates a comma-separated list of keys
+        set_list = [str(key) for key in self.dictionary]
+        return f'MySet: {{{", ".join(set_list)}}}'
